59889082136ca17a345bca0c1d2134d381068f0e
ThiagoSawada <thiagokitajima@gmail.com>
2026-03-03 16:48:47 -0300

    x

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	float x, resultado;
+	
+	printf("digite o valor de x para a conta 8/2 - x: ");
+	scanf("%f", &x);
+	
+	resultado = 8/2 - x;
+	
+	printf("o resultado da conta é: %.2f", resultado);
+return 0;
+}
